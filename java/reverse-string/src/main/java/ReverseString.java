b091881eb548a382818d995aee94e4c94c8bc4df
softish <lazar.h.klincov@live.se>
2020-07-15 23:27:21 +0200

    Solution for Exercise: reverse-string, Iteration 2

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,11 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class ReverseString {
 
     String reverse(String inputString) {
-        char[] input = inputString.toCharArray();
-        char[] reversed = new char[input.length];
-        for (int inputPos = 0, reversedPos = input.length - 1; inputPos < input.length; inputPos++, reversedPos--) {
-            reversed[reversedPos] = input[inputPos];
+        Deque<Character> inputCharacters = new ArrayDeque<>();
+        for(Character character : inputString.toCharArray()) {
+            inputCharacters.push(character);
         }
-        return String.valueOf(reversed);
+
+        StringBuilder reversedCharacters = new StringBuilder();
+        while (!inputCharacters.isEmpty()) {
+            reversedCharacters.append(inputCharacters.pop());
+        }
+
+        return reversedCharacters.toString();
     }
 }
