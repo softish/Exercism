703e186a63d8cc7b67cafe271e6dd3a5d5b03635
softish <lazar.h.klincov@live.se>
2020-08-13 22:56:25 +0200

    Solution for Exercise: reverse-string, Iteration 6

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,15 +1,9 @@
 class ReverseString {
 
-    /**
-     * inspired by <a href="https://www.baeldung.com/java-invert-array#using-a-traditional-for-loop">Baeldung</a>
-     */
     String reverse(String inputString) {
-        char[] chars = inputString.toCharArray();
-        for (int i = 0; i < chars.length / 2; i++) {
-            char tmp = chars[i];
-            chars[i] = chars[chars.length - 1 - i];
-            chars[chars.length - 1 - i] = tmp;
-        }
-        return String.valueOf(chars);
+        return new StringBuilder()
+                .append(inputString)
+                .reverse()
+                .toString();
     }
 }
