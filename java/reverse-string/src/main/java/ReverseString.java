f06b9a3cfca3f652472454e4cfd6b349c87d8bd4
softish <lazar.h.klincov@live.se>
2020-08-13 22:35:11 +0200

    Solution for Exercise: reverse-string, Iteration 4

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,40 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 class ReverseString {
 
     String reverse(String inputString) {
-        Stack<Character> inputCharacters = new Stack<>();
-        for (Character character : inputString.toCharArray()) {
-            inputCharacters.push(character);
+        return reverse(inputString, 0, "");
+    }
+
+    private String reverse(String inputString, int index, String reverseString) {
+        if (index == inputString.length()) {
+            return reverseString;
         }
 
-        StringBuilder reversedCharacters = new StringBuilder();
-        while (!inputCharacters.isEmpty()) {
-            reversedCharacters.append(inputCharacters.pop());
-        }
-
-        return reversedCharacters.toString();
-    }
-}
-
-class Stack<E> {
-
-    private final Deque<E> elements;
-
-    public Stack() {
-        elements = new ArrayDeque<>();
-    }
-
-    public void push(E element) {
-        elements.push(element);
-    }
-
-    public E pop() {
-        return elements.pop();
-    }
-
-    public boolean isEmpty() {
-        return elements.isEmpty();
+        return reverse(inputString, index + 1, inputString.charAt(index) + reverseString);
     }
 }
