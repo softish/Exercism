acd0a11d6cfe77f72648daf3cb1b840e444ed00b
softish <lazar.h.klincov@live.se>
2020-08-17 21:44:08 +0200

    Solution for Exercise: reverse-string, Iteration 8

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,8 +1,48 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class ReverseString {
 
     String reverse(String inputString) {
-        return new StringBuilder(inputString)
-                .reverse()
-                .toString();
+        SimpleStack<Character> inputCharacters = new SimpleStack<>();
+        for (Character character : inputString.toCharArray()) {
+            inputCharacters.push(character);
+        }
+
+        List<Character> reversedString = new ArrayList<>();
+        while (!inputCharacters.isEmpty()) {
+            reversedString.add(inputCharacters.pop());
+        }
+
+        return reversedString.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+}
+
+/**
+ * Facade for non-synchronized java collections stack implementation.
+ */
+class SimpleStack<E> {
+
+    private final Deque<E> elements;
+
+    public SimpleStack() {
+        elements = new ArrayDeque<>();
+    }
+
+    public void push(E element) {
+        elements.push(element);
+    }
+
+    public E pop() {
+        return elements.pop();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
     }
 }
