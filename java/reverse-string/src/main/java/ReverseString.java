3d1d7cbb17c948a28345ba570530c23b856fc85e
softish <lazar.h.klincov@live.se>
2020-08-17 21:45:48 +0200

    Solution for Exercise: reverse-string, Iteration 9

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,48 +1,15 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class ReverseString {
 
+    /**
+     * Inspired by <a href="https://www.baeldung.com/java-invert-array#using-a-traditional-for-loop">Baeldung</a>
+     */
     String reverse(String inputString) {
-        SimpleStack<Character> inputCharacters = new SimpleStack<>();
-        for (Character character : inputString.toCharArray()) {
-            inputCharacters.push(character);
+        char[] chars = inputString.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = tmp;
         }
-
-        List<Character> reversedString = new ArrayList<>();
-        while (!inputCharacters.isEmpty()) {
-            reversedString.add(inputCharacters.pop());
-        }
-
-        return reversedString.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining());
-    }
-}
-
-/**
- * Facade for non-synchronized java collections stack implementation.
- */
-class SimpleStack<E> {
-
-    private final Deque<E> elements;
-
-    public SimpleStack() {
-        elements = new ArrayDeque<>();
-    }
-
-    public void push(E element) {
-        elements.push(element);
-    }
-
-    public E pop() {
-        return elements.pop();
-    }
-
-    public boolean isEmpty() {
-        return elements.isEmpty();
+        return String.valueOf(chars);
     }
 }
