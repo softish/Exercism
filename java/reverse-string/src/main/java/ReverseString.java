932eaf39ee4968f396d2a62ea7f8746784f32fc5
softish <lazar.h.klincov@live.se>
2020-08-17 21:50:59 +0200

    Solution for Exercise: reverse-string, Iteration 10

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,15 +1,23 @@
 class ReverseString {
 
+    private static final int BEGINNING = 0;
+    private static final int ONE_BASED_INDEX = 1;
+    private static final int HALF = 2;
+
     /**
      * Inspired by <a href="https://www.baeldung.com/java-invert-array#using-a-traditional-for-loop">Baeldung</a>
      */
     String reverse(String inputString) {
         char[] chars = inputString.toCharArray();
-        for (int i = 0; i < chars.length / 2; i++) {
+        for (int i = BEGINNING; i < chars.length / HALF; i++) {
             char tmp = chars[i];
-            chars[i] = chars[chars.length - 1 - i];
-            chars[chars.length - 1 - i] = tmp;
+            chars[i] = chars[getZeroBasedIndexLength(chars) - i];
+            chars[getZeroBasedIndexLength(chars) - i] = tmp;
         }
         return String.valueOf(chars);
+    }
+
+    private int getZeroBasedIndexLength(char[] chars) {
+        return chars.length - ONE_BASED_INDEX;
     }
 }
