840f091b3e566082e9842e678c435a2a121b5a36
softish <lazar.h.klincov@live.se>
2021-06-12 23:00:20 +0200

    Solution for Exercise grains, Iteration 5

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -5,22 +5,58 @@ import java.util.concurrent.ConcurrentHashMap;
 class Grains {
 
     private static final int FIRST_SQUARE = 1;
-    private static final int LAST_SQUARE = 64;
-    private static final int TOTAL_KEY = 0;
+    public static final int LAST_SQUARE = 64;
 
-    private final Map<Integer, BigInteger> grainsCalculationsCache = new ConcurrentHashMap<>();
+    private final SquareCalculator squareCalculator;
+    private final BoardCalculator boardCalculator;
+
+    public Grains() {
+        squareCalculator = new SquareCalculator();
+        boardCalculator = new BoardCalculator();
+    }
 
     BigInteger grainsOnSquare(final int square) {
         if (square < FIRST_SQUARE || LAST_SQUARE < square) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
 
-        return grainsCalculationsCache.computeIfAbsent(square, key -> BigInteger.TWO.pow(key - 1));
+        return squareCalculator.calculateGrains(square);
     }
 
     BigInteger grainsOnBoard() {
-        return grainsCalculationsCache.computeIfAbsent(TOTAL_KEY, ignore -> BigInteger.TWO
-                .pow(LAST_SQUARE)
-                .subtract(BigInteger.ONE));
+        return boardCalculator.calculateGrains();
+    }
+}
+
+abstract class CachedGrainCalculator {
+
+    private static final int TOTAL_KEY = 0;
+
+    private final Map<Integer, BigInteger> grainsCalculationsCache = new ConcurrentHashMap<>();
+
+    public BigInteger calculateGrains(int square) {
+        return grainsCalculationsCache.computeIfAbsent(square, ignore -> calculateNumberOfGrains(square));
+    }
+
+    public BigInteger calculateGrains() {
+        return calculateGrains(TOTAL_KEY);
+    }
+
+    abstract BigInteger calculateNumberOfGrains(int square);
+}
+
+class SquareCalculator extends CachedGrainCalculator {
+    @Override
+    BigInteger calculateNumberOfGrains(int square) {
+        return BigInteger.TWO.pow(square - 1);
+    }
+}
+
+class BoardCalculator extends CachedGrainCalculator {
+    @Override
+    BigInteger calculateNumberOfGrains(int square) {
+        return BigInteger.TWO
+                .pow(Grains.LAST_SQUARE)
+                .subtract(BigInteger.ONE);
     }
 }
