0e5d10d010428684142e849b0b3763936238b31f
softish <lazar.h.klincov@live.se>
2021-06-12 22:32:49 +0200

    Solution for Exercise grains, Iteration 4

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -8,19 +8,18 @@ class Grains {
     private static final int LAST_SQUARE = 64;
     private static final int TOTAL_KEY = 0;
 
-    private final Map<Integer, BigInteger> gransPerSquareCache = new ConcurrentHashMap<>();
-    private final Map<Integer, BigInteger> grainsTotalCache = new ConcurrentHashMap<>();
+    private final Map<Integer, BigInteger> grainsCalculationsCache = new ConcurrentHashMap<>();
 
     BigInteger grainsOnSquare(final int square) {
         if (square < FIRST_SQUARE || LAST_SQUARE < square) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
 
-        return gransPerSquareCache.computeIfAbsent(square, ignore -> BigInteger.TWO.pow(square - 1));
+        return grainsCalculationsCache.computeIfAbsent(square, key -> BigInteger.TWO.pow(key - 1));
     }
 
     BigInteger grainsOnBoard() {
-        return grainsTotalCache.computeIfAbsent(TOTAL_KEY, ignore -> BigInteger.TWO
+        return grainsCalculationsCache.computeIfAbsent(TOTAL_KEY, ignore -> BigInteger.TWO
                 .pow(LAST_SQUARE)
                 .subtract(BigInteger.ONE));
     }
